ddcf7c094b26cdf9cd163f2398a02fe9155f5056
Rafael Rodrigues da Silva <rafaelx7t@hotmail.com>
2022-05-20 18:41:38 -0300

    Primeiro Commit Base

---
+++ src/test/java/br/com/perinity/test/gerenciamentotarefas/GerenciamentoTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.perinity.test.gerenciamentotarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
